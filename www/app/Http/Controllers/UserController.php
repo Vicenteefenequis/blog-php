f12f3fcf99738fe7aa3b5d88ad0ce5f91ecb0c7d
Vicente Junior <vicente19981@live.com>
2021-08-23 20:17:46 -0300

    feat: criado controlador de usuario

--- www/app/Http/Controllers/UserController.php
+++ www/app/Http/Controllers/UserController.php
@@ -12,17 +12,50 @@ class UserController extends Controller
     }
 
     public function store(Request $request){
-        $validateData = $this->validate($request,
+        $validatedData = $this->validate($request,
             [
-                "name" => "required",
-                "email" => "required","email address"
+                "name" => "required|max:255",
+                'email' => 'required|max:255|email|unique:users,email',
+                "password" => "required"
             ]
         );
+        $validatedData['password'] = \Hash::createBcryptDriver()->make($validatedData['password']);
 
-
-
+        $user = User::create($validatedData);
+        $user->refresh();
+        return $user;
     }
 
+    public function update(Request $request,$id){
+        $user = User::findOrFail($id);
+
+        $validatedData = $this->validate($request,[
+            "name" => "max:255",
+            "password"=>"required",
+            "new_password"=>"max:255",
+            "confirm_new_password"=>"same:new_password"
+        ]);
+
+        if(!\Hash::check($validatedData["password"],$user->password)){
+            return response()->json(["message"=>"Invalid Credentials"],401);
+        }
+
+        $validatedData['password'] = $user->updatePassword($validatedData['new_password'] ?? null);
+
+        $user->update($validatedData);
+        $user->refresh();
+        return $user;
+    }
+
+    public function show($id){
+        return User::findOrFail($id);
+    }
+
+    public function destroy($id){
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->noContent();
+    }
 
 
 }
