96bc7bf173244dda1fbd510cd778f3a22d72ec95
Vicente Junior <vicente19981@live.com>
2021-08-23 17:30:49 -0300

    feature: criado arquivo de inicializacao do laravel

---
+++ www/app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        return User::all();
+    }
+
+    public function store(Request $request){
+        $validateData = $this->validate($request,
+            [
+                "name" => "required",
+                "email" => "required","email address"
+            ]
+        );
+
+
+
+    }
+
+
+
+}
