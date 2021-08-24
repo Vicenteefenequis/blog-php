d3d9da848632cc1ca3877c6e98faba03c5ea842e
Vicente Junior <vicente19981@live.com>
2021-08-23 21:53:45 -0300

    refactor: removendo import desnecessarios

--- www/app/Models/Category.php
+++ www/app/Models/Category.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\Category
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category query()
+ * @mixin \Eloquent
+ */
 class Category extends Model
 {
     use HasFactory, HasFactory, Notifiable;
