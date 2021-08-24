b92a5eb169f102e42df1c060416446ba941d9f37
Vicente Junior <vicente19981@live.com>
2021-08-23 21:58:15 -0300

    feat: criado rotas para post

--- www/routes/api.php
+++ www/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::resource("users", UserController::class);
 Route::resource("categories", CategoryController::class);
+Route::resource("posts", PostController::class);
