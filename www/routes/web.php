256227a2aaecf8d799fd8cbf3b58b9415bf537da
Vicente Junior <vicente19981@live.com>
2021-08-30 20:16:40 -0300

    feat: addded base url of gateway of docker internal

--- www/routes/web.php
+++ www/routes/web.php
@@ -13,13 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$baseUrl = 'gateway.docker.internal:8000/api';
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/app');
 });
 
 
-Route::get('/posts',function (){
-    return view('posts');
+Route::get('/posts', function () use ($baseUrl) {
+    $response = \Illuminate\Support\Facades\Http::acceptJson()->get($baseUrl . '/posts')->json();
+    list('data' => $posts,'links' => $links, 'meta' => $meta) = $response;
+
+    return view('posts', [
+        'posts' => $posts,
+        'links' => $links,
+        'meta' => $meta
+    ]);
+
 });
 
 Route::get('/dashboard', function () {
