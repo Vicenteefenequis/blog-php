c7d96853887ed3a27befeb6d7948757a55648ad9
Vicente Junior <vicente19981@live.com>
2021-08-31 21:14:15 -0300

    refactor: change for max character

--- www/database/factories/PostFactory.php
+++ www/database/factories/PostFactory.php
@@ -23,7 +23,7 @@ class PostFactory extends Factory
     {
         return [
             "title" => $this->faker->sentence(4),
-            "description" => $this->faker->text(155)
+            "description" => $this->faker->text(3000)
         ];
     }
 }
