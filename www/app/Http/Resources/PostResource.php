61cfdbecc97c6a77349c079b5d07a5ea4748936e
Vicente Junior <vicente19981@live.com>
2021-09-07 20:45:20 -0300

    refactor: adding of photo url

--- www/app/Http/Resources/PostResource.php
+++ www/app/Http/Resources/PostResource.php
@@ -6,19 +6,18 @@ use App\Models\Category;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
-{
+class PostResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
-    public function toArray($request)
-    {
+    public function toArray($request) {
         return [
             "id" => $this->id,
             "title" => $this->title,
+            "photo_url" => stripslashes($this->photo_url),
             "description" => $this->description,
             "category" => Category::find($this->category_id),
             "user" => User::find($this->user_id),
